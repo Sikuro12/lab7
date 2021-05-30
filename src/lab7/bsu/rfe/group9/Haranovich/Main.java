f248df80af78d5f6bd482bad6497c1908b27fe1d
Haranovich Valiantsin <Sikuro12@yandex.by>
2021-05-30 22:17:27 +0300

    main

---
+++ src/lab7/bsu/rfe/group9/Haranovich/Main.java
@@ -0,0 +1,16 @@
+package lab7.bsu.rfe.group9.Haranovich;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                final MainFrame frame = new MainFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
